1b1a2bf974ac6f998a3a51e2dd7a3e6c14332cb8
HeoChauA <caothehai1991@gmail.com>
2016-05-21 20:15:45 +0700

    update

--- wp-content/themes/wfly-theme/js/script.js
+++ wp-content/themes/wfly-theme/js/script.js
@@ -1,11 +1,21 @@
 (function($) {
-  function test() {
-    //alert('ok');
+  function navigation() {
+    $('.menu-icon').click(function() {
+      $('ul.main-nav').slideToggle('fast');
+    });
+
+    var parent_item = $('ul.main-nav li.menu-item-has-children > a');
+    parent_item.append('<i class="fa fa-angle-down submenu-icon"></i>');
+    $('.submenu-icon').click(function(){
+      var this_parent = $(this).parent('a');
+      this_parent.next('ul.nav-drop').slideToggle('fast');
+      return false;
+    });
   }
 
   $(document).ready(function() {
     // Call to function
-    test();
+    navigation();
   });
 
   $(window).load(function() {
